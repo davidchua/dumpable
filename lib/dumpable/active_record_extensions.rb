d5714c78690d38c66ef780ae2d92803f0d137439
David Chua <zhchua@gmail.com>
2021-07-22 01:40:01 +0000

    Update dumpable for postgres and also allow automatic association generation

--- lib/dumpable/active_record_extensions.rb
+++ lib/dumpable/active_record_extensions.rb
@@ -7,7 +7,51 @@ module Dumpable
     end
 
     module ClassMethods
-      def dumpable(options={})
+        cattr_accessor :root, :seen_before, :blacklist
+
+        def get_class_from_name(class_name)
+          begin
+            return class_name.constantize
+          rescue NameError => e
+            puts "Nameerror hit, cannot find Class: #{e.inspect}"
+            self.const_get(class_name.to_sym)
+          rescue Exception => e
+            raise "Exception getting class_from_name: #{e.inspect}"
+          end
+        end
+
+        def all_associations
+          root = []
+          class_names = self.reflect_on_all_associations.map(&:name) - [:versions]
+          self.reflect_on_all_associations.each do |m|
+            begin
+              hh = {}
+              hh[m.name] = self.get_class_from_name(m.class_name).dump_associations.compact - [:versions]
+              root << hh
+            rescue Exception => e
+              puts e.inspect
+            end
+
+          end
+          return root
+        end
+
+        def dump_associations
+          self.reflect_on_all_associations.map do |m|
+            begin
+              if m.name == :versions
+                next
+              end
+
+              m.name
+            rescue Exception => e
+              puts "#{e.inspect}"
+              next
+            end
+          end
+        end
+
+        def dumpable(options={})
         class_eval do
           cattr_accessor :dumpable_options
         end
--- lib/dumpable/dumper.rb
+++ lib/dumpable/dumper.rb
@@ -1,8 +1,10 @@
 module Dumpable
   class Dumper
     attr_accessor :dumpee, :options, :id_padding, :dumps
+    attr_accessor :done
 
     def initialize(dumpee, options={})
+      @done = []
       @dumpee = dumpee
       @options = Dumpable.config.merge(options || {})
       @id_padding = @options[:id_padding] || (@dumpee.class.respond_to?(:dumpable_options) && @dumpee.class.dumpable_options[:id_padding]) || Dumpable.config.id_padding
@@ -11,24 +13,28 @@ module Dumpable
     end
 
     def dump
+#      puts "DUMP: %#{@dumps.inspect}"
       recursive_dump(@dumpee, @dumps)
-      @lines << generate_insert_query(@dumpee)
+      @lines = [generate_insert_query(@dumpee)]
+      Dumpable::FileWriter.write(@lines.flatten.compact.reverse, options)
     end
 
     def self.dump(*records_and_collections)
       options = records_and_collections.extract_options!
-      lines = []
+#      lines = []
       records_and_collections.each do |record_or_collection|
         if record_or_collection.is_a?(Array) || record_or_collection.is_a?(ActiveRecord::Relation) || (record_or_collection.is_a?(Class) && record_or_collection.ancestors.include?(ActiveRecord::Base))
           record_or_collection = record_or_collection.all if record_or_collection.is_a?(Class) && record_or_collection.ancestors.include?(ActiveRecord::Base)
           record_or_collection.each do |object|
-            lines << new(object, options).dump
+            lines = new(object, options).dump
+#            Dumpable::FileWriter.write(lines.flatten.compact.reverse, options)
           end
         else
-          lines << new(record_or_collection, options).dump
+          lines = new(record_or_collection, options).dump
+#      Dumpable::FileWriter.write(lines.flatten.compact.reverse, options)
         end
       end
-      Dumpable::FileWriter.write(lines.flatten.compact, options)
+      #Dumpable::FileWriter.write(lines.flatten.compact.reverse, options)
     end
 
     private
@@ -37,7 +43,7 @@ module Dumpable
 
       elsif dumps.is_a?(Array)
         dumps.each do |mini_dump|
-          recursive_dump(object, mini_dump)
+           recursive_dump(object, mini_dump)
         end
       elsif dumps.is_a?(Hash)
         dumps.each do |key, value|
@@ -45,18 +51,28 @@ module Dumpable
           Array(object.send(key)).each { |child| recursive_dump(child, value) }
         end
       elsif dumps.is_a?(Symbol) || dumps.is_a?(String)
+				# iterate through
+
         Array(object.send(dumps)).each do |child_object|
+
           reflection = object.class.reflect_on_association(dumps.to_sym)
-          if reflection.macro == :belongs_to
-            object.send("#{reflection.association_foreign_key}=", object.id + @id_padding)
-          elsif [:has_many, :has_one].include? reflection.macro
-            if reflection.respond_to?(:foreign_key)
-              child_object.send("#{reflection.foreign_key}=", object.id + @id_padding)
-            else
-              child_object.send("#{reflection.primary_key_name}=", object.id + @id_padding)
-            end
-          end
-          @lines << generate_insert_query(child_object)
+#          opposite_reflection = child_object.class.reflect_on_association(object.class.name.downcase.to_sym)
+
+#        take note that this current version of dumpable doesn't handle padding since we're going to disable padding below
+
+#        if reflection.macro == :belongs_to
+#          puts "\n\n\n\n\nhere #{reflection.macro} - #{object.inspect} - #{reflection.inspect}"
+#          object.send("#{reflection.foreign_key}=", object.id + @id_padding)
+#        # has_many should update both sides
+#        elsif [:has_many, :has_one].include? reflection.macro
+#          if reflection.respond_to?(:foreign_key)
+#            object.send("#{reflection.foreign_key}=", object.id + @id_padding)
+#          else
+#            object.send("#{reflection.primary_key_name}=", object.id + @id_padding)
+#          end
+#        end
+          @lines = [generate_insert_query(child_object)]
+          Dumpable::FileWriter.write(@lines.flatten.compact.reverse, options)
         end
       end
     end
@@ -67,34 +83,58 @@ module Dumpable
       cloned_attributes = object.attributes.clone
       return nil unless cloned_attributes["id"].present?
       cloned_attributes["id"] += @id_padding
+
       key_values = cloned_attributes.collect do |key,value|
-        [key, dump_value_string(value)] unless skip_columns.include?(key.to_s)
+        # check for enum
+        if object.defined_enums.has_key?(key)
+          value = object.class.send(key.pluralize)[value]
+        end
+        next if !object.class.column_names.include?(key)
+        [key, dump_value_string_from_helper(object, key, value)] unless skip_columns.include?(key.to_s)
       end.compact
-      keys = key_values.collect{ |item| "`#{item[0]}`" }.join(", ")
+      keys = key_values.collect{ |item| "#{item[0]}" }.join(", ")
       values = key_values.collect{ |item| item[1].to_s }.join(", ")
 
-      "INSERT INTO #{object.class.table_name} (#{ keys }) VALUES (#{ values });"
+      "INSERT INTO #{object.class.table_name} (#{ keys }) VALUES (#{ values }) ON CONFLICT DO NOTHING;"
+    end
+
+    def dump_value_string_from_helper(dumpable_object,key, value)
+          begin
+            value = ActiveRecord::Relation::QueryAttribute.new(
+              key,
+              dumpable_object.attributes_before_type_cast[key],
+              dumpable_object.class.type_for_attribute(key)
+            )
+
+            ActiveRecord::Base.connection.quote(value.value_for_database)
+          rescue ActiveRecord::SerializationTypeMismatch => e
+            return ActiveRecord::Base.connection.quote(dumpable_object.attributes_before_type_cast[key])
+          end
     end
 
     # http://invisipunk.blogspot.com/2008/04/activerecord-raw-insertupdate.html
     def dump_value_string(value)
       case value.class.to_s
         when "Time", "ActiveSupport::TimeWithZone"
-          "'#{value.strftime("%Y-%m-%d %H:%M:%S")}'"
+          "\'#{value.strftime("%Y-%m-%d %H:%M:%S")}\'"
         when "NilClass"
           "NULL"
-        when "Fixnum"
+        when "Fixnum" || "Integer"
           value
         when "String"
-          "'#{value.gsub(/'/, "\\\\'")}'"
+          "E\'#{value.gsub(/'/, "\\\\'")}\'"
         when "FalseClass"
-          '0'
+          "\'0\'"
         when "TrueClass"
-          '1'
+          "\'1\'"
         when "ActiveSupport::HashWithIndifferentAccess"
-          "'#{value.to_yaml.gsub(/'/, "\\\\'")}'"
+          "\'#{value.to_yaml.gsub(/'/, "\\\\'")}\'"
+        when "Array"
+          "E\'#{value}\'"
+        when "Hash"
+          "\'#{value.to_json}\'"
         else
-          "'#{value}'"
+          "\'#{value}\'"
       end
     end
   end
